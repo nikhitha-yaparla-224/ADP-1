6f95907618089d7d919892047ee974bb01b4a7ba
nikhitha-yaparla-224 <24cse.yaparlanikhitha@skct.edu.in>
2024-11-23 10:04:26 +0530

    Add files via upload

---
+++ nikki/4.js
@@ -0,0 +1,16 @@
+<html>
+<head>
+    <title>java script</title>
+    <script>
+        var a,b,c;
+        a=parseInt(prompt("Enter a value"));
+        b=parseInt(prompt("Enter a value"));
+        c=a+b;
+        window.alert(sum of " + a + "and" + b +  "is" + c);
+        window.confirm("Thankl You");
+
+
+    </script>
+
+</head>
+</html>
---
+++ nikki/newFile.js
@@ -0,0 +1,14 @@
+<><html /><head>
+    <title>java script</title>
+    <script>
+        var a,b,c;
+        a=parseInt(prompt("Enter a value"));
+        b=parseInt(prompt("Enter b value"));
+        c=a+b;
+        window.alert("sum of " + a + "and" + b +  "is" + c);
+        window.confirm("Thank You");
+
+
+    </script>
+</head><body>
+    </body></>;
